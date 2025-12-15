ceb62cd7e09ef9bdc9dda0b89024b10cd689ea47
Gajanan1904 <152101821+Gajanan1904@users.noreply.github.com>
2025-12-15 13:21:26 +0530

    Next greater element

---
+++ Next Greater Element.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+void NextGreaterElement(int arr[],int ans[],int n){
+    stack<int>s;
+    int idx=n-1;
+    ans[idx]=-1;
+    s.push(arr[idx]);
+    
+    for(int i=idx-1;i>=0;i--){
+        int CurrEle=arr[i];
+        while(!s.empty() && CurrEle>=s.top()){
+            s.pop();
+        }
+        if(s.empty()){
+            ans[i]=-1;
+        }
+        else{
+            ans[i]=s.top();
+        }
+        s.push(CurrEle);
+    }
+    
+    for(int i=0;i<n;i++){
+        cout<<ans[i]<<" ";
+    }
+    cout<<endl;
+}
+int main(){
+    int n;
+    cin>>n;
+    int arr[n],ans[n];
+    
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+        ans[i]=0;
+    }
+    NextGreaterElement(arr,ans,n);
+    return 0;
+}
