4758de51d25035794cedfef3e3e34168aa95ed10
Gajanan1904 <152101821+Gajanan1904@users.noreply.github.com>
2025-12-19 15:45:20 +0530

    Remove Nth Node into LinkedList

---
+++ RemoveNthNodeInLinkedList.cpp
@@ -0,0 +1,111 @@
+// You are using GCC
+#include<iostream>
+using namespace std;
+
+class Node{
+public:
+    int data;
+    Node *next;
+
+    Node(int val){
+        data = val;
+        next = NULL;
+    }
+};
+
+class List{
+public:
+    Node *head;
+    Node *tail;
+
+    List(){
+        head = tail = NULL;
+    }
+
+    void push_front(int val){
+        Node *newNode = new Node(val);
+        if(head == NULL){
+            head = tail = newNode;
+        } else {
+            newNode->next = head;
+            head = newNode;
+        }
+    }
+
+    void push_back(int val){
+        Node *newNode = new Node(val);
+        if(head == NULL){
+            head = tail = newNode;
+        } else {
+            tail->next = newNode;
+            tail = newNode;
+        }
+    }
+
+    void pop_front(){
+        Node *temp = head;          
+        head = head->next;          
+        temp->next = NULL;
+        delete temp;
+    }
+
+    void pop_back(){
+        Node *temp = head;          
+        while(temp->next->next != NULL){ 
+            temp = temp->next;
+        }
+        delete tail;
+        tail = temp;
+        tail->next = NULL;
+    }
+
+    int getsize(){
+        Node *temp = head;
+        int sz = 0;
+
+        while(temp != 0){           
+            temp = temp->next;
+            sz++;
+        }
+        return sz;
+    }
+
+    void RemoveNthNode(int n){
+        int size = getsize();
+
+        Node *temp = head;          
+
+        for(int i = 1; i < (size - n); i++){
+            temp = temp->next;      
+        }
+
+        Node *todel = temp->next;   
+        cout << "Going to delete: " << todel->data << endl;
+        temp->next = temp->next->next;
+        
+    }
+
+    void printList(){
+        Node *temp = head;
+        while(temp != NULL){
+            cout << temp->data << " ";
+            temp = temp->next;
+        }
+        cout << endl;
+    }
+};
+
+int main(){
+    List ll;
+    ll.push_front(3);
+    ll.push_front(2);
+    ll.push_front(1);
+    
+
+    ll.push_back(4);
+    ll.push_back(5);
+    ll.printList();
+
+    ll.RemoveNthNode(3);
+    ll.printList();
+}
