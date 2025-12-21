eb5cfe2903dfebd70b4be42cdeedd4595d00bc6b
Gajanan1904 <152101821+Gajanan1904@users.noreply.github.com>
2025-12-21 16:24:30 +0530

    Inter leave Queue

---
+++ InterleaveQueue.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<queue>
+using namespace std;
+void InterleaveQueue(queue<int>&originalQ){
+    int n=originalQ.size()/2;
+    queue<int>tempQ;
+    for(int i=0;i<n;i++){
+        tempQ.push(originalQ.front());
+        originalQ.pop();
+    }
+    
+    while(!tempQ.empty()){
+        originalQ.push(tempQ.front());
+        tempQ.pop();
+        
+        originalQ.push(originalQ.front());
+        originalQ.pop();
+    }
+}
+int main(){
+    queue<int>originalQ;
+    for(int i=1;i<=10;i++){
+        originalQ.push(i);
+    }
+    
+    InterleaveQueue(originalQ);
+    
+    while(!originalQ.empty()){
+        cout<<originalQ.front()<<" ";
+        originalQ.pop();
+    }
+}
