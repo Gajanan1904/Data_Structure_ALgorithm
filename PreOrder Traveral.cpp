e5bcb7e03978d3d1b0b7f81da5317324402ed4c7
Gajanan1904 <152101821+Gajanan1904@users.noreply.github.com>
2025-12-22 20:25:14 +0530

    PreOrder Traversal

---
+++ PreOrder Traveral.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node *left;
+    Node *right;
+    Node(int data){
+        left=right=NULL;
+    }
+};
+static int idx=-1;
+Node *BuildTree(int arr[],int n){
+    idx++;
+    if(idx>=n || idx==-1){
+        return NULL;
+    }
+    Node *currNode=new Node(arr[idx]);
+    currNode->left=BuildTree(arr,n);
+    currNode->right=BuildTree(arr,n);
+    
+    return currNode;
+}
+
+void PreOrder(Node *root){
+    if(root==NULL){
+        return;
+    }
+    cout<<root->data<<" ";
+    PreOrder(root->left);
+    PreOrder(root->right);
+}
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    Node *root=BuildTree(arr,n);
+    cout<<"PreOrder Traversal: ";
+    PreOrder(root);
+    
+}
