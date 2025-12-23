7e7ae311a371131541d4d9ae7a345128225d0098
Gajanan1904 <152101821+Gajanan1904@users.noreply.github.com>
2025-12-23 09:07:17 +0530

    Post Order Traversal

---
+++ PostOrder Traversal.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node *left;
+    Node *right;
+    Node(int data){
+        this->data=data;
+        left=right=NULL;
+    }
+};
+static int idx=-1;
+Node *BuildTree(int arr[],int n){
+    idx++;
+    if(idx>=n || arr[idx]==-1){
+        return NULL;
+    }
+    Node *currNode=new Node(arr[idx]);
+    currNode->left=BuildTree(arr,n);
+    currNode->right=BuildTree(arr,n);
+    
+    return currNode;
+}
+
+void PostOrder(Node *root){
+    if(root==NULL){
+        return;
+    }
+    PostOrder(root->left);
+    PostOrder(root->right);
+    cout<<root->data<<" ";
+    
+}
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    Node *root=BuildTree(arr,n);
+    cout<<"PostOrder Traversal: ";
+    PostOrder(root);
+    
+}
