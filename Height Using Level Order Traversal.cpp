da454b1fc34586a1656f09b52da86fa00aaa5621
Gajanan1904 <152101821+Gajanan1904@users.noreply.github.com>
2025-12-25 21:18:39 +0530

    Height Using Level Order Traversal

--- Height Using Level Order Traversal.cpp
+++ Height Using Level Order Traversal.cpp
@@ -56,7 +56,7 @@ void LevelWiseTraversal(Node *root){
     cout<<"total height of tree is: "<<count;
 }
 int main(){
-    int n;
+    int n,a;
     cin>>n;
     int arr[n];
     for(int i=0;i<n;i++){
