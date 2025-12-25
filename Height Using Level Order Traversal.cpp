6138df10d6d5bb1e131c0a0d246e2b9db223bf0b
Gajanan1904 <152101821+Gajanan1904@users.noreply.github.com>
2025-12-25 21:17:56 +0530

    Height Using Level Order Traversal

---
+++ Height Using Level Order Traversal.cpp
@@ -0,0 +1,68 @@
+#include<iostream>
+#include<queue>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node *left;
+    Node *right;
+    Node(int data){
+        this->data=data;
+        left=right=NULL;
+    }
+};
+static int idx=-1;
+Node *BuildTree(int arr[],int n){
+      idx++;
+      if(idx>=n || arr[idx]==-1){
+          return NULL;
+      }
+      Node *currNode=new Node(arr[idx]);
+      currNode->left=BuildTree(arr,n);
+      currNode->right=BuildTree(arr,n);
+      
+      return currNode;
+}
+
+void LevelWiseTraversal(Node *root){
+    int count=0;
+    if(root==NULL){
+        return;
+    }
+    queue<Node *>q;
+    q.push(root);
+    q.push(NULL);
+    while(!q.empty()){
+        Node *curr=q.front();
+        q.pop();
+        if(curr==NULL){
+            count++;
+            if(q.empty()){
+                break;
+            }
+            q.push(NULL);
+            
+            
+        }
+        else{
+            if(curr->left!=NULL){
+                q.push(curr->left);
+            }
+            if(curr->right!=NULL){
+                q.push(curr->right);
+            }
+        }
+    }
+    cout<<"total height of tree is: "<<count;
+}
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    
+    Node *root=BuildTree(arr,n);
+    LevelWiseTraversal(root);
+}
