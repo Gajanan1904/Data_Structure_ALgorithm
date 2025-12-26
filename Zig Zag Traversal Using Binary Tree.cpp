d6ee1856de505d797a65d94548e24a10411dcc43
Gajanan1904 <152101821+Gajanan1904@users.noreply.github.com>
2025-12-26 15:17:37 +0530

    Zig Zag Traversal

--- Zig Zag Traversal Using Binary Tree.cpp
+++ Zig Zag Traversal Using Binary Tree.cpp
@@ -50,7 +50,7 @@ vector<vector<int>> zigzagLevelOrder(Node* root) {
 int main() {
     Node* root = new Node(1);
     root->left = new Node(2);
-    
+    root->right = new Node(3);
 
     vector<vector<int>> ans = zigzagLevelOrder(root);
 
