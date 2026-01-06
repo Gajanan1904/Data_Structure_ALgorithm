c27e655bb6f7e937f3dd422d29c6a4874e8bacd9
Gajanan1904 <152101821+Gajanan1904@users.noreply.github.com>
2026-01-06 22:28:07 +0530

    Post Order

--- PostOrder Traversal.cpp
+++ PostOrder Traversal.cpp
@@ -35,7 +35,8 @@ void PostOrder(Node *root){
 int main(){
     int n;
     cin>>n;
-    
+    int arr[n];
+
     Node *root=BuildTree(arr,n);
     cout<<"PostOrder Traversal: ";
     PostOrder(root);
