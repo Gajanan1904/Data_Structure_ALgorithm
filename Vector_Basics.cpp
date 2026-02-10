1bfd7248a305a2dd5393e5f63160a563c15f5e45
Gajanan1904 <152101821+Gajanan1904@users.noreply.github.com>
2026-02-10 21:46:40 +0530

    Vector_Basics

---
+++ Vector_Basics.cpp
@@ -0,0 +1,87 @@
+// #include<iostream>
+// #include<vector>
+// using namespace std;
+// int main(){
+//     //methods of initialisation
+//     vector <int> vec1; 
+//     vector <int> vec2={1,2,3,5,6};
+//     vector <int> vec3(10,-1);
+//     //to couunt size of array
+//     cout<<vec1.size()<<endl;
+//     cout<<vec2.size()<<endl;
+//     cout<<vec3.size()<<endl;
+    
+// }
+
+// #include<iostream>
+// #include<vector>
+// using namespace std;
+// int main(){
+//     //methods of initialisation
+//     vector <int> vec3(10,-1);
+//     //to print elements of vector here the element is -1 so we will get -1 -1 -1 -1... as O/P
+//   for(int i=0;i<vec3.size();i++){
+//       cout<<vec3[i]<<" ";
+//   }
+//     cout<<endl;
+// }
+
+//TO ADD OR DELETE ELEMENTS IN vector
+// #include<iostream>
+// #include<vector>
+// using namespace std;
+// int main(){
+//     vector <int> vec1={1,2,3,4};
+//     cout<<"Size: "<<vec1.size()<<endl;
+//     cout<<"Capacity: "<<vec1.size()<<endl;
+    
+//     vec1.push_back(5);
+    
+//     cout<<"Size: "<<vec1.size()<<endl;
+//     cout<<"Capacity: "<<vec1.capacity();
+// }
+
+
+//TO ADD AND THEN PRINT THE vector
+// #include<iostream>
+// #include<vector>
+// using namespace std;
+// int main(){
+//     vector <int> vec1={1,2,3,4};
+   
+    
+//     vec1.push_back(5);
+//     vec1.push_back(6);
+//     for(int i=0;i<vec1.size();i++){
+//         cout<<vec1[i]<<" ";
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
