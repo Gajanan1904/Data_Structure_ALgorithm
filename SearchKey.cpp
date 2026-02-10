1a96374daf31472b0195139f5bf7b59ec7b8fe03
Gajanan1904 <152101821+Gajanan1904@users.noreply.github.com>
2026-02-10 21:47:44 +0530

    Search

--- SearchKey.cpp
+++ SearchKey.cpp
@@ -98,12 +98,7 @@ class List{
 };
 int main(){
     List ll;
-    ll.push_front(3);
-    ll.push_front(2);
-    ll.push_front(1);
-    ll.push_front(0);
-    ll.push_back(4);
-    ll.push_back(5);
+   
 
     ll.InsertAtPos(0,4);
     ll.printList();             
