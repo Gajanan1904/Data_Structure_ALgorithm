5a56354f71b7347ded0f5a45827998dbe089c6eb
Gajanan Bidwai <152101821+Gajanan1904@users.noreply.github.com>
2026-02-12 23:31:43 +0530

    Patterns

---
+++ patterns.cpp
@@ -0,0 +1,80 @@
+ //Pattern 1:BOX PATTERN
+//  #include<iostream>
+//  using namespace std;
+//  int main(){
+//      int n;
+//      cin>>n;
+//      for(int i=1;i<=n;i++){
+//          for(int j=1;j<=n;j++){
+//              cout<<" "<<i ;
+//          }
+//          cout<<"\n";
+//      }
+//  }
+
+// Pattern 2:Right angled triangle
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cin>>n;
+//     for(int i=1;i<=n;i++){
+//         for(int j=1;j<=i;j++){
+//             cout<<i;
+//         }
+//     cout<<endl;
+//     }
+// }
+
+//Pattern 3:inverted right angled triangle
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cin>>n;
+//     for(int i=1;i<=n;i++){
+//         for(int j=1;j<=(n-i+1);j++){
+//             cout<<"*";
+//         }
+//     cout<<endl;
+//     }
+// }
+
+// Pattern 4:Half pyramid pattern
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cin>>n;
+//     for(int i=1;i<=n;i++){
+//         for(int j=1;j<=i;j++){
+//             cout<<j;
+//         }
+//         cout<<endl;
+//     }
+// }
+
+//Pattern 5:Character Pyramid Pattern
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cin>>n;
+//     char ch='A';
+//     for(int i=1;i<=n;i++){
+//         for(int j=1;j<=i;j++){
+//             cout<<ch;
+//             ch++;
+//         }
+//         cout<<endl;
+//     }
+//}
+
+
+
+
+
+
+
+
+
