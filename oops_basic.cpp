938b920a22a118a07fb8e19c77b01289e16bd293
Gajanan Bidwai <152101821+Gajanan1904@users.noreply.github.com>
2026-02-14 23:09:24 +0530

    OOP's

---
+++ oops_basic.cpp
@@ -0,0 +1,59 @@
+//Access modifiers
+// #include<iostream>
+// using namespace std;
+// class Student{
+//     public:
+//     string name;
+//     float cgpa;
+    
+//     void getPercentage(){
+//         cout<<(cgpa*10)<<endl;
+//     }
+// };
+// int main(){
+//     Student s1;
+//     s1.name="Omii";
+//     s1.cgpa=9.0;
+//     cout<<s1.name<<endl;
+//     cout<<s1.cgpa<<endl;
+//     s1.getPercentage();
+    
+// }
+
+//Setters & Getterzs
+// #include<iostream>
+// using namespace std;
+// class Student{
+//     private:
+//     string name;
+//     float cgpa;
+//     public:
+//     void setName(string nameval){
+//         name=nameval;
+//     }
+    
+//     void setCgpa(float cgpaval){
+//         cgpa=cgpaval;
+//     }
+    
+//     string getName(){
+//         cout<<name;
+//         return name;
+//     }
+    
+//     float getCgpa(){
+//         cout<<cgpa;
+//         return cgpa;
+//     }
+    
+// };
+// int main(){
+//     Student s1;
+//     s1.setName("Omii");
+//     s1.setCgpa(9.5);
+//     s1.getName();
+//     cout<<endl;
+//     s1.getCgpa();
+    
+// }
+
