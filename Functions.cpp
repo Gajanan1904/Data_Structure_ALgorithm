b58e5b1663165eb695d34d5c3450f6f96ea84247
Gajanan Bidwai <152101821+Gajanan1904@users.noreply.github.com>
2026-02-22 23:45:20 +0530

    commit

---
+++ Functions.cpp
@@ -0,0 +1,104 @@
+// // #include<iostream>
+// // using namespace std;
+// // void myfunc(){
+// //     cout<<"HELLO";
+// // }
+// // int main(){
+// //     myfunc();
+// // }
+
+// // #include<iostream>
+// // using namespace std;
+// // void func1(){
+// //     cout<<"HEELO F1\n";
+// // }
+// // void func2(){
+// //     func1();
+// //     cout<<"WORK DONE";
+// // }
+// // int main(){
+// //     func2();
+// // }
+
+// // #include<iostream>
+// // using namespace std;
+// // void sayHello();
+// // int main(){
+// //     sayHello();
+// // }
+// // void sayHello(){
+// //     cout<<"HELLO";
+// // }
+
+// // #include<iostream>
+// // using namespace std;
+// // int sum(int a,int b){
+// //     int sum=a+b;
+// //     return sum;
+// // }
+// // int main(){
+// //     int s=sum(5,6);
+// //     cout<<"THE SUM IS "<<s;
+// //     return 0;
+// // }
+
+// // #include<iostream>
+// // using namespace std;
+// // int sub(int a,int b){
+// //     int sub=a-b;
+// //     return sub;
+// // }
+// // int main(){
+// //     int s=sub(6,5);
+// //     cout<<"THE SUB IS "<<s;
+// //     return 0;
+// // }
+
+// // #include<iostream>
+// // using namespace std;
+// // int product(int a,int b){
+// //     int prod=a * b;
+// //     return prod;
+// // }
+// // int main(){
+// //     int P=product(5,4);
+// //     cout<<"THE PRODUCT OF A & B IS "<<P;
+// //     return 0;
+// // }
+
+// // #include<iostream>
+// // using namespace std;
+// // bool iseven(int n){
+// //     if(n%2==0){
+// //         return true;
+// //     }
+// //     else{
+// //         return false;
+// //     }
+// // }
+// // int main(){
+// //     cout<<iseven(55);
+//     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
