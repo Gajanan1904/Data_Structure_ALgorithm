1bacab0161a127f785bd69bf90a53a8b15e2f7ce
Gajanan Bidwai <152101821+Gajanan1904@users.noreply.github.com>
2026-03-11 23:16:25 +0530

    commit

--- SearchKey.cpp
+++ SearchKey.cpp
@@ -101,7 +101,10 @@ int main(){
     ll.push_front(3);      
     ll.push_front(2);      
     ll.push_front(1);      
-    ll.push_back(4);    
+    ll.push_back(4);
+    ll.push_back(5);    
+
+    
     ll.InsertAtPos(0,4);
     ll.printList();             
     ll.SearchKey(0);
