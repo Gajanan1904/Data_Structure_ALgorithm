f75fab6b90570c085317ae55bae10a50513dd965
Gajanan Bidwai <152101821+Gajanan1904@users.noreply.github.com>
2026-03-13 23:36:19 +0530

    commit

--- LevelOrder Traversal.cpp
+++ LevelOrder Traversal.cpp
@@ -48,7 +48,9 @@ int main(){
     int n;
     cin>>n;
     int arr[n];
-    for(int i=0;i<n;i++)
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
     
     Node *root=BuildTree(arr,n);
     cout<<"Level Order Traversal: ";
