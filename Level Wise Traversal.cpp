9004286ad4a697ef4e5fc8d282e7e9b38e9e561e
Gajanan Bidwai <152101821+Gajanan1904@users.noreply.github.com>
2026-03-18 23:12:04 +0530

    commit

--- Level Wise Traversal.cpp
+++ Level Wise Traversal.cpp
@@ -53,11 +53,6 @@ void LevelWiseTraversal(Node *root){
 }
 int main(){
     int n;
-    cin>>n;
-    int arr[n];
-    for(int i=0;i<n;i++){
-        cin>>arr[i];
-    }
 
     
     Node *root=BuildTree(arr,n);
