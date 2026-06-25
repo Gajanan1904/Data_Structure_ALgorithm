2ff326f4d37e0c752073ec9f9c2d8e6c7dbac711
Gajanan Bidwai <152101821+Gajanan1904@users.noreply.github.com>
2026-06-25 10:52:16 +0530

    commit

--- Summer_PEP.cpp
+++ Summer_PEP.cpp
@@ -59,3 +59,27 @@ int main() {
         cout << s << endl;
     }
 }
+
+//Count Unique of a grid
+#include <iostream>
+using namespace std;
+
+int uniquePaths(int i, int j, int m, int n) {
+    if (i == m - 1 && j == n - 1)
+        return 1;
+
+    if (i >= m || j >= n)
+        return 0;
+
+    return uniquePaths(i + 1, j, m, n) +
+           uniquePaths(i, j + 1, m, n);
+}
+
+int main() {
+    int m, n;
+    cin >> m >> n;
+
+    cout << uniquePaths(0, 0, m, n);
+
+    return 0;
+}
