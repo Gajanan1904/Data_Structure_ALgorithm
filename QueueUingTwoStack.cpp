685e634eb666203f887c9ca5e2453a2cda5df8e6
Gajanan Bidwai <152101821+Gajanan1904@users.noreply.github.com>
2026-08-04 23:02:18 +0530

    commmit

--- QueueUingTwoStack.cpp
+++ QueueUingTwoStack.cpp
@@ -33,11 +33,7 @@ class Queue{
     }
 };
 int main(){
-    Queue q;
-    q.push(1);
-    q.push(2);
-    q.push(3);
-    q.push(4);
+    
     
     while(!q.empty()){
         cout<<q.front()<<endl;
