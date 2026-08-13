012f4296594ba1db11c5acc02b00873b8f6c316a
Gajanan Bidwai <152101821+Gajanan1904@users.noreply.github.com>
2026-08-13 23:40:53 +0530

    commit

--- Linear_Search_Array.cpp
+++ Linear_Search_Array.cpp
@@ -1,12 +1,7 @@
 #include<iostream>
 using namespace std;
 int linearsearch(int *arr,int n,int key){
-    for(int i=0;i<n;i++){
-        if(arr[i]==key){
-            return i;
-        }
-    }
-    return -1;
+    
     
 }
 int main(){
