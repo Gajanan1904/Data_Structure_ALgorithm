995b62afdb50d1c2842b5bf3daa159d082ab2c74
Gajanan Bidwai <152101821+Gajanan1904@users.noreply.github.com>
2026-08-30 23:29:17 +0530

    commit

--- Inorder Traversal.cpp
+++ Inorder Traversal.cpp
@@ -36,7 +36,9 @@ int main(){
     int n;
     cin>>n;
     int arr[n];
-    
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
     Node *root=BuildTree(arr,n);
     cout<<"InOrder Traversal: ";
     InOrder(root);
