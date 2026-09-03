7907668b2c0a1aa0cf4a8193135a2c99b015f2e3
Gajanan Bidwai <152101821+Gajanan1904@users.noreply.github.com>
2026-09-03 23:57:21 +0530

    commit

--- Inheritance.cpp
+++ Inheritance.cpp
@@ -199,14 +199,9 @@
 //     b1.breath();
 //     b1.fly();
 
-//     Fish f1;
-//     f1.color = "Blue";
-//     cout << "\nFish color: " << f1.color << endl;
-//     f1.eat();
-//     f1.breath();
-//     f1.swim();
+//     
 
-
+ 
 
 //     return 0;
 // }
