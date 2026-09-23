5f5abfbc9b2a34acb041d4d59cd1842fac4e0d56
Gajanan Bidwai <152101821+Gajanan1904@users.noreply.github.com>
2026-09-23 18:01:56 +0530

    commit

--- Height Of Tree.cpp
+++ Height Of Tree.cpp
@@ -36,7 +36,12 @@ int height(Node *root){
     
 }
 int main(){
-    
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
     
     Node *root=BuildTree(arr,n);
     int heightTree=height(root);
